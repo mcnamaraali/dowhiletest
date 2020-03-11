e7366d6736b701467dba4d7154ec0b73162b9556
mcnamaraali <mcnamaraali@gmail.com>
2020-03-11 14:47:05 +0100

    Do while test
    A simple do while test java script

---
+++ Dowhiletest.java
@@ -0,0 +1,18 @@
+package doWhileTest;
+
+
+public class Dowhiletest {
+
+	public static void main(String[] args) {
+		int counter = 1;
+		
+		do {
+			System.out.printf("%d ", counter);
+			++counter;
+			
+		} while (counter <= 10);
+			
+		System.out.println();
+	}
+
+}
